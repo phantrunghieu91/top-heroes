745efc13751641d25f92c1d4484b569945b95d08
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2025-03-07 15:08:17 +0700

    fixed hero shard calc

--- src/components/HeroShardCalculator.vue
+++ src/components/HeroShardCalculator.vue
@@ -3,12 +3,14 @@ import heroStars from '@/assets/heroes-stars.jpg';
 import Container from './Container.vue';
 import InputGroup from './InputGroup.vue';
 import TabsContentHeading from './TabsContentHeading.vue';
+import TabsContentFormWrapper from './TabsContentFormWrapper.vue';
 
 import { ref } from 'vue';
 const tier = ref('legendary');
-const shardPerStep = ref([0, 1, 1, 2, 2, 4, 6, 4, 4, 8, 8, 16, 4, 8, 16, 16]);
+const shardPerStep = ref([1, 1, 2, 2, 4, 6, 4, 4, 8, 8, 16, 4, 8, 16, 16]);
 const stars = ref({
   start: 0,
+  process: 0,
   end: 0,
 });
 const totalShards = ref(0);
@@ -16,6 +18,7 @@ const fullStarStep = 5;
 const calculateShards = () => {
   let start = stars.value.start;
   let end = stars.value.end;
+  let process = stars.value.process;
 
   if (start === end) {
     totalShards.value = 0;
@@ -27,73 +30,51 @@ const calculateShards = () => {
     alert('Current star should be less than wanted star');
     return;
   }
-  // if end is decimal, round it to the nearest whole number
-  if (end % 1 > 0) {
-    end = Math.ceil(end);
-    stars.value.end = end;
-  }
-
-  // start and end decimal part cant greater than 5
-  if (start % 1 > 0.5 || end % 1 > 0.5) {
-    totalShards.value = 0;
-    alert('Star decimal part should be less than 0.5');
-    // reset the input
-    stars.value = { start: 0, end: 0 };
-    return;
-  }
 
   let total = 0;
-  let currentStar = Math.floor(start);
-  let wantedStar = Math.floor(end);
-  let currentStarDecimal = Math.floor(start % 1 * 10);
-  if (currentStarDecimal === 5) {
-    currentStarDecimal = 0;
-    stars.value.start = currentStar + 1;
-    currentStar += 1;
-  }
-  // console.log('currentStarDecimal: ', currentStarDecimal);
-  for (let i = currentStarDecimal === 0 ? currentStar + 1 : currentStar; i <= wantedStar; i++) {
+  
+  for (let i = start; i < end; i++) {
     let tierMulti = tier.value === 'legendary' ? 1 : 2;
     let shard = shardPerStep.value[i];
     let shardPerStar = shard * fullStarStep * tierMulti;
     let remainingShardForCurrentStar = 0;
-    if (i === currentStar && currentStarDecimal > 0) {
-      remainingShardForCurrentStar = currentStarDecimal * shard * tierMulti;
-      console.log('have remain: ', remainingShardForCurrentStar);
+    if (i === start && process > 0) {
+      remainingShardForCurrentStar = process * shard * tierMulti;
       shardPerStar -= remainingShardForCurrentStar;
     }
-    // console.log(`${i}: `, shardPerStar, remainingShardForCurrentStar);
     total += shardPerStar;
   }
 
   totalShards.value = total;
 }
 const resetInputs = () => {
-  stars.value = { start: 0, end: 0 };
+  stars.value = { start: 0, process: 0, end: 0 };
   totalShards.value = 0;
 }
+const colNum = 4;
 </script>
 <template>
   <Container>
     <TabsContentHeading :title="'Heroes Shard Calculator'"/>
-    <div class="py-4 grid grid-cols-1 md:grid-cols-3 gap-2">
+    <TabsContentFormWrapper :col-num="colNum">
       <InputGroup label="Select Tier" id="tier" type="select" :options="['legendary', 'mythic']" v-model="tier" />
-      <InputGroup label="Enter current star (e.g, 5.1)" id="current-star" type="number" v-model="stars.start"
-        :number-options="{ step: 0.1, min: 0, max: 15 }" />
+      <InputGroup label="Enter current star" id="current-star" type="number" v-model="stars.start"
+        :number-options="{ step: 1, min: 0, max: 15 }" />
+      <InputGroup label="Current star process (eg. 1 / 5 of star)" id="current-star-process" type="select" :options="['0', '1', '2', '3', '4']" v-model="stars.process"/>
       <InputGroup label="Enter your wanted star (e.g, 15)" id="wanted-star" type="number" v-model="stars.end"
-        :number-options="{ step: 0.1, min: 0, max: 15 }" />
-      <div class="md:col-span-3 bg-white p-4 rounded shadow grid grid-cols-2 md:grid-cols-4 gap-4">
-        <span class="capitalize">Hero Tier: {{ tier }}</span>
+        :number-options="{ step: 1, min: 0, max: 15 }" />
+      <div class="col-span-2 md:col-span-4 bg-white p-4 rounded shadow grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div class="capitalize">Hero Tier: <span :class="tier === 'legendary' ? 'text-amber-500' : 'text-red-600'">{{ tier }}</span></div>
         <span>Current Star: {{ stars.start }}</span>
         <span>Wanted Star: {{ stars.end }}</span>
-        <span>Total Shards: {{ totalShards }}</span>
+        <span class="text-rose-700">Total Shards: {{ totalShards }}</span>
       </div>
-      <div class="flex justify-center gap-4 md:col-span-3">
+      <div class="flex justify-center gap-4 col-span-2 md:col-span-4">
         <button type="button" class="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"
           @click="calculateShards">Calculate</button>
         <button type="reset" class="px-4 py-2 bg-gray-400 rounded cursor-pointer" @click="resetInputs">Reset</button>
       </div>
-    </div>
+    </TabsContentFormWrapper>
     <div class="flex justify-center">
       <img :src="heroStars" alt="Heroes Stars" class="w-fit" />
     </div>
--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -12,14 +12,14 @@ type Props = {
 };
 const props = defineProps<Props>()
 const model = defineModel()
-const classes = 'w-full p-2 border border-gray-300 rounded bg-gray-100'
+const classes = 'w-full p-2 border border-gray-300 rounded bg-gray-100 leading-0 self-end'
 </script>
 <template>
-  <div class="bg-white p-4 rounded shadow">
+  <div class="bg-white p-4 rounded shadow flex flex-col h-full justify-between">
     <label :for="id" class="block mb-2">{{ label }}</label>
     <input v-if="type == 'number'" v-model="model" :id="id" type="number" :step="numberOptions?.step" :min="numberOptions?.min" :max="numberOptions?.max" required
       :class="classes" />
-    <select v-model="model" :id="id" :class="classes" v-if="type == 'select'">
+    <select v-model="model" :id="id" :class="classes" v-if="type == 'select'" class="capitalize">
       <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
     </select>
   </div>
